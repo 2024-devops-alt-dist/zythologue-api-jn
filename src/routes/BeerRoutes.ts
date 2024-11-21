b1c32df2ee97e5b9779f5755aeb63f8812a901f2
jimni6 <j.ni@betclicgroup.com>
2024-11-21 14:33:53 +0100

    feat: add delete beer functionality

--- src/routes/BeerRoutes.ts
+++ src/routes/BeerRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {createBeer, getBeerById, getBeers, updateBeer} from '../controllers/BeerController';
+import {createBeer, deleteBeer, getBeerById, getBeers, updateBeer} from '../controllers/BeerController';
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get('/beers', getBeers);
 router.get('/beers/:id', getBeerById);
 router.post('/beers/', createBeer);
 router.put('/beers/:id', updateBeer);
+router.delete('/beers/:id', deleteBeer);
 
 export default router;
