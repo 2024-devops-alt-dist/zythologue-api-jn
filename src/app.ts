3df602d7b6e8aaddf1ca819523c4106ec3c56d41
j-mmyni <j.ni@betclicgroup.com>
2025-01-14 10:47:21 +0100

    fix: cors authorization for local front

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express from 'express'
+// @ts-ignore
+import cors from 'cors';
 import breweryRoutes from './routes/BreweryRoutes'
 import beerRoutes from './routes/BeerRoutes'
 import {swaggerSpec, swaggerUi} from "./swagger";
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5174', // Frontend URL
+}));
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(breweryRoutes);
